eba7fa95a478232149b972a6bd61fa134d681412
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2025-09-09 10:20:41 -0700

    omg please

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  base: "/",
   server: {
     port: 5173,
     open: true,
     proxy: {
-      "/api":  { target: "http://localhost:3000", changeOrigin: true },
-    //   "/rsvp": { target: "http://localhost:3000", changeOrigin: true },
-    //   "/admin":{ target: "http://localhost:3000", changeOrigin: true }
+      "/api": { target: "http://localhost:3000", changeOrigin: true }
     }
   },
-  base: "/"
+  build: {
+    rollupOptions: {
+      input: {
+        main:  resolve(__dirname, "index.html"),
+        rsvp:  resolve(__dirname, "rsvp.html"),
+        admin: resolve(__dirname, "admin.html")
+      }
+    }
+  }
 });
